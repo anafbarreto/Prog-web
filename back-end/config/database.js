8f667cdb44ae4701567d690b2bc1f8d9cfe6a581
ANA CAROLINA FONSECA BARRETO <1090481823001@fatec.local>
2020-03-02 22:08:54 -0300

    Aula MongoDB

--- back-end/config/database.js
+++ back-end/config/database.js
@@ -11,4 +11,14 @@ module.exports = function(uri) {
     mongoose.connection.on('disconnected', () =>
     console.log(`Mongoose! desconectado de ${uri}`)
     )
+    mongoose.connection.on('error', erro =>
+    console.log(`Mongoose! ERRO ao conectar ${uri}: {erro}`)
+    )
+    process.on('SIGINT', () =>
+        mongoose.Connection.CLOSE(( ) => {
+            Console.log(`Mongoose! desconectado pelo termino da aplicação`)
+            process.exit(0) // 0 é a saida sem erros
+        })
+    )    
+
 }
