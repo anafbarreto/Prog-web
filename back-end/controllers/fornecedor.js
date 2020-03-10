0edf69040cf26045a66d1f778a39c262291fd4a2
ANA CAROLINA FONSECA BARRETO <1090481823001@fatec.local>
2020-03-09 22:14:20 -0300

    Manipulação no BD

--- back-end/controllers/fornecedor.js
+++ back-end/controllers/fornecedor.js
@@ -2,6 +2,8 @@ const Fornecedor = require('../models/Fornecedor')
 
 const controller = {} //objeto vazio
 
+
+// criando novos dados de fornecedor no BD  
 controller.novo = async (req, res) => {
     try {
         await Fornecedor.create(req.body)  //HTTP Status 201: Created
@@ -9,6 +11,75 @@ controller.novo = async (req, res) => {
     }
     catch (erro) {
         console.log(erro) //HTTP 500: Internal Server Error
+        res.status(500).send(erro)
+    }
+
+}
+
+// buscar todos os dados do banco
+controller.listar = async (req, res) => {
+    try {
+        const lista = await Fornecedor.find() //find() sem parametros, retorna todos os fornecedores que estão no banco 
+        res.send(lista)
+    }
+
+    catch{
+        console.log(erro)
+        res.status(500).send(erro)
+    }
+}
+
+// selecionar dados especifico no banco de dados
+controller.obterUm = async (req, res) => {
+    try {
+        const id = req.params.id
+        const obj = await Fornecedor.findById(id) //find() com parametros, retorna busca especifica que está no banco 
+        if (obj) {     // para objeto encontrado
+            res.send(obj) // http 200 implícito
+        }
+        else {
+            res.status(404).end() // http 404: not found
+        }
+    }
+    catch{
+        console.log(erro)
+        res.status(500).send(erro)
+    }
+}
+
+//atualizar dados no banco
+controller.atualizar = async (req, res) => {
+    try {
+        const id = req.body._id
+        const obj = await Fornecedor.findByIdAndUpdate(id, req.body)
+        if (obj) {  // para objeto encontrado e atualizado
+            res.status(204).end()
+        }
+        else {
+            res.status(404).end() // http 404: not found
+        }
+    }
+    catch (erro) {
+        console.log(erro)
+        res.status(500).send(erro)
+    }
+
+}
+
+// deletando do BD
+controller.delete = async (req, res) => {
+    try {
+        const id = req.body._id
+        const obj = await Fornecedor.findByIdAndDelete(id)
+        if (obj) {  // para objeto encontrado e atualizado
+            res.status(204).end()
+        }
+        else {
+            res.status(404).end() // http 404: not found
+        }
+    }
+    catch (erro) {
+        console.log(erro)
         res.status(500).send(erro)
     }
 
--- back-end/routes/Fornecedor.js
+++ back-end/routes/Fornecedor.js
@@ -3,7 +3,11 @@ const router = express.Router();
 const controller = require('../controllers/fornecedor')
 
 router.post('/', controller.novo)
+router.get('/', controller.listar)
+router.put('/', controller.atualizar) 
+router.delete('/', controller.delete)
+router.get('/:id', controller.obterUm) //aqui selecionamos o que vem depois da barra e definimos como id
 
 module.exports = router
-
+ 
 
