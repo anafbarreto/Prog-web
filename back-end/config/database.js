a6cce37ed08aa945d174905910a7a9471a20a09f
Ana Carolina Fonseca Barreto <KAROLBARRETO10@GMAIL.COM>
2020-05-17 20:29:49 -0300

    Sistema biblioteca 17/05

--- back-end/config/database.js
+++ back-end/config/database.js
@@ -1,25 +1,30 @@
 const mongoose = require('mongoose')
-module.exports = function(uri) {
-    mongoose.connect(uri, {
-        useNewUrlParser:true,
-        useUnifiedTopology: true,
-        useFindAndModify: false
-    })
 
-    mongoose.connection.on('connected', () =>
-    console.log(`Mongoose! conectado a ${uri}`)
-    )
-    mongoose.connection.on('disconnected', () =>
-    console.log(`Mongoose! desconectado de ${uri}`)
-    )
-    mongoose.connection.on('error', erro =>
-    console.log(`Mongoose! ERRO ao conectar ${uri}: {erro}`)
-    )
-    process.on('SIGINT', () =>
-        mongoose.Connection.CLOSE(( ) => {
-            Console.log(`Mongoose! desconectado pelo termino da aplicação`)
-            process.exit(0) // 0 é a saida sem erros
-        })
-    )    
+module.exports = function(uri) {
+   mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true
+   })
+
+   mongoose.connection.on('connected', () =>
+      console.log(`Mongoose! conectado a ${uri}`)
+   )
+
+   mongoose.connection.on('disconnected', () =>
+      console.log(`Mongoose! desconectado de ${uri}`)
+   )
+
+   mongoose.connection.on('error', erro =>
+      console.log(`Mongoose! ERRO ao conectar a ${uri}: ${erro}`)
+   )
+
+   process.on('SIGINT', () => 
+      mongoose.connection.close(() => {
+         console.log('Mongoose! desconectado pelo término da aplicação')
+         process.exit(0) // 0 => saída sem erros
+      })
+   )
 
 }
