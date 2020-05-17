d4fb27f8ee07d98f52717b28477cabff2e9e514a
Ana Carolina Fonseca Barreto <KAROLBARRETO10@GMAIL.COM>
2020-05-17 20:38:08 -0300

    Manipulação MongoDB - Sistema Biblioteca 17/05

--- back-end/routes/teste.js
+++ back-end/routes/teste.js
@@ -3,8 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Olá mundo!!');
+  res.send('Olá, pessoal!');
 });
-
 
 module.exports = router;
