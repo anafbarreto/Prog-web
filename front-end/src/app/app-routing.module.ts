6994920b4686d8243e2549355b5f6c0c87b1d34d
Ana Carolina Fonseca Barreto <KAROLBARRETO10@GMAIL.COM>
2020-06-22 19:37:16 -0300

    Ligação CRUD front-end

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FornecedorListComponent } from './fornecedor/fornecedor-list/fornecedor-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'fornecedor', //no angular não usa barra (/) antes do nome da rota
+    component: FornecedorListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
